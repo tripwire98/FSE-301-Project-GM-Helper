db54a7082cf462510bda18224bb493a2a3dea9b2
rkenney3 <66690174+rkenney3@users.noreply.github.com>
2020-06-14 14:44:33 -0700

    Create Scene.cs

---
+++ GM Helper/GM Helper/Code Folder/Scene.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Ajax.Utilities;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GM_Helper.Code_Folder
+{
+    public class Scene
+    {
+        public TimeOfDay time;
+        public Location location;
+        public int numberOfPeople;
+        public String NPCs;
+        public String periodDescription;
+        public String sceneDescription;
+
+
+        public String CreateSceneDescription()
+        {
+            //TBD
+            String description = "";
+            return description;
+        }
+
+        public void SetLocation(Location loc)
+        {
+            location = loc;
+        }
+
+        public void SetTime(TimeOfDay t)
+        {
+            time = t;
+        }
+
+
+        public void SetNumberOfPeople(int n)
+        {
+            numberOfPeople = n;
+        }
+
+    }
+}
